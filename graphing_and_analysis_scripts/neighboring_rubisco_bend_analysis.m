a56466625cbfb1af3e6f4ce43dcd2abe98e53cbb
lee2637 <17flee@roadrunner.com>
2025-01-16 16:31:12 -0500

    Add files via upload

--- graphing_and_analysis_scripts/neighboring_rubisco_bend_analysis.m
+++ graphing_and_analysis_scripts/neighboring_rubisco_bend_analysis.m
@@ -101,22 +101,17 @@ function [] = neighboring_rubisco_bend_analysis(carboxysome_data, bin_limit, bin
     % Make labels for the x axis of the format [-45,-40), for example.
     % Matlab puts values on the edge of two bins in the larger bin, so we
     % use an open parenthesis on the right
-    for i = 1:length(custom_bins) - 1
-        if i == length(custom_bins) - 1
-            % the largest bin gets a closed parenthesis on the right
-            this_label = ['[',num2str(custom_bins(i)),',',num2str(custom_bins(i+1)),']'];
-        else
-            this_label = ['[',num2str(custom_bins(i)),',',num2str(custom_bins(i+1)),')'];
-        end
+    for i = 1:6
+        % the largest bin gets a closed parenthesis on the right
+        this_label = num2str(10 * (i-1));
 
         x_axis_labels{end+1} = this_label; % add label to list of labels
     end
-    xticks(1:length(custom_bins) - 1); % make enough ticks for each bin
+    xticks(0:5:25); % make enough ticks for each bin
     xticklabels(x_axis_labels); % load the x axis labels
     
     % create and edit the colorbar
+    caxis([min(z_values), max(z_values)]);
     c = colorbar('Location', 'southoutside');
     c.Label.String = 'Inner Rubisco Concentration (\muM)'; % colorbar title
-    c.Ticks = [0, 0.25, 0.5, 0.75, 1];
-    c.TickLabels = round(linspace(min(plotz, [], "all"), max(plotz, [], "all"), 5), 0);
 end
